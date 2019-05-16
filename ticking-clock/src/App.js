eef5c868ab5a50b411df842b80c6c1d128f45067
varikagoel18 <varikagoel18@gmail.com>
2019-05-16 21:09:03 +0530

    ticking clock webapp

---
+++ ticking-clock/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    time: new Date().toLocaleTimeString()
+  };
+
+  updateTimeHandler = () => {
+    this.setState({
+      time: new Date().toLocaleTimeString()
+    });
+  };
+  render() {
+    {
+      setInterval(this.updateTimeHandler, 1000);
+    }
+
+    return (
+      <div className="App">
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.time} .</h2>
+      </div>
+    );
+  }
+}
+
+//setInterval(App, 100);
+export default App;

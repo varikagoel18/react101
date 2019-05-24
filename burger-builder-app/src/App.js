2f22c9aba734c1ba24da112d53e5e3c8a2579109
varikagoel18 <varikagoel18@gmail.com>
2019-05-24 23:10:07 +0530

    basic proj structure

--- burger-builder-app/src/App.js
+++ burger-builder-app/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
+import BurgerBilder from "./containers/BurgerBuilder/BurgerBuilder";
 
-function App() {
-  return <div />;
+class App extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <BurgerBilder />
+      </Layout>
+    );
+  }
 }
 
 export default App;
---
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Aux from "../../hoc/Aux";
+
+class BurgerBilder extends React.Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger live</div>
+        <div>Burger ingredients to Add</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBilder;
